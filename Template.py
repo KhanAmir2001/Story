fbe227f9a061f6c2294fb1a286eed830c99a5c1b
Amir Khan <khana253@coventry.ac.uk>
2020-10-01 11:07:31 +0100

    uploaded a template we can all put our code in

---
+++ Template.py
@@ -0,0 +1,25 @@
+def part1():
+    print("test")
+    return()
+
+def part2():
+
+    return()
+
+def part3():
+
+    return()
+
+def part4():
+
+    return()
+
+def part5():
+
+    return()
+
+part1()
+part2()
+part3()
+part4()
+part5()
