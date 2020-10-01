b6048cb0d8bd8e7d06637e66be49a536742ae45b
Luqmaan Syed <syedl@coventry.ac.uk>
2020-10-01 10:48:12 +0100

    testing file upload

---
+++ PythonStoryPart5.py
@@ -0,0 +1,14 @@
+#Part 5 - The shutdown Start
+print('\n"Loading"')
+print('"Loading"')
+print('"Loading"\n')
+print('"Successfully loaded"')
+input("\nPress enter to continue...\n")
+print("You open your eyes onto a new World.\nIt says welcome to Fackbook.com.\nYou look around only to find there's no room - it's absolutely crowded\n")
+
+input("Press enter to continue...\n")
+print("You see all sorts of stuff in this world.\nThere are a lot pictures with text on them - they call them memes or something of the sort.\nYou can hear people talking to each other from all over the world - it's really impressive.\n")
+input("Press enter to continue...\n")
+print("Wait what is happening? The world is slowing down, you can see empty boxes - they used to be images of people.\n")
+print("What is going on?!\n") 
+print("I'm frozen!")
