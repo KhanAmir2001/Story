3455e671c7d800a343d882b7fbef96105ad4a737
Luqmaan Syed <syedl@coventry.ac.uk>
2020-10-01 11:21:29 +0100

    Luqmaan Pt5 Done

--- Template.py
+++ Template.py
@@ -16,6 +16,33 @@ def part4():
 
 def part5():
 
+        print('\n"Loading"')
+        print('"Loading"')
+        print('"Loading"\n')
+        print('"Successfully loaded"')
+
+        input("\nPress enter to continue...\n")
+
+        print("You open your eyes onto a new World.\nIt says welcome to Fackbook.com.\nYou look around only to find there's no room - it's absolutely crowded\n")
+
+        input("Press enter to continue...\n")
+
+        print("You see all sorts of stuff in this world.\nThere are a lot pictures with text on them - they call them memes or something of the sort.\nYou can hear people talking to each other from all over the world - it's really impressive.\n")
+
+        input("Press enter to continue...\n")
+
+        print("Wait what is happening? The world is slowing down, you can see empty boxes - they used to be images of people.\n")
+        print("What is going on?!\n") 
+        print("I'm frozen!\n")
+
+        print("Some kind of message appears...\n")
+        print('"Warning: the site is experiencing some technical difficulties.\nPlease hold on while our engineers work to fix the issue - :D\n"')
+        input("Press enter to continue...\n")
+        print("You wait for a bit\n")
+        input("Press enter to continue...\n")
+        print('"Warning: the site has officially shut down due to a DDOS attack.\nWe are sorry of the inconvenience.\nPlease accept our apologies and try later today or tomorrow"')
+        
+
     return()
 
 part1()
